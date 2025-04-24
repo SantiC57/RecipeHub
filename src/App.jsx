70237b4f90c64471a5fbe40b1f0991bb3c5de5cd
Tomas0418 <tomas.giraldoc@comunidad.iush.edu.co>
2025-04-24 13:29:22 -0500

    Agregando correctamente las carpetas Images y Pages con su contenido

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Signup from "./components/Signup/Signup.jsx";
 import Login from "./Login.jsx";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import MainPage from "./MainPage.jsx";
+import Pastas from "./Pages/Pastas/Pastas.jsx";
+import Carnes from "./Pages/Carnes/Carnes.jsx";
+import Mariscos from "./Pages/Mariscos/Mariscos.jsx"; // Asegúrate de que las rutas sean correctas
 
 function DynamicTitle() {
   const location = useLocation();
@@ -13,6 +16,9 @@ function DynamicTitle() {
       "/": "Recipehub",
       "/login": "Iniciar sesión",
       "/signup": "Registro",
+      "/pastas": "Recetas de Pastas", // Asegúrate de que sea minúscula
+      "/carnes": "Recetas de Carnes",
+      "/mariscos": "Recetas de Mariscos", // Asegúrate de que sea minúscula
     };
 
     document.title = titles[location.pathname] || "Recipehub";
@@ -29,6 +35,9 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/pastas" element={<Pastas />} /> {/* Asegúrate de que la ruta sea minúscula */}
+        <Route path="/carnes" element={<Carnes />} /> {/* Asegúrate de que la ruta sea minúscula */}
+        <Route path="/mariscos" element={<Mariscos />} /> {/* Asegúrate de que la ruta sea minúscula */}
       </Routes>
     </Router>
   );
---
+++ src/Pages/Mariscos/Mariscos.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./mariscos.css";
+import Paella from "../../images/paella.jpg"; // Ruta corregida
+import CoctelCamaron from "../../images/coctel-camaron.jpg"; // Ruta corregida
+import ChupeCentolla from "../../images/chupe-centolla.jpg"; // Ruta corregida
+
+
+const Mariscos = () => {
+  return (
+    <div className="categoria">
+      <h2 className="categoria__titulo">Recetas de Mariscos</h2>
+      <div className="categoria__lista">
+        <div className="receta">
+          <img src={Paella} alt="Paella" />
+          <h3>Paella</h3>
+        </div>
+        <div className="receta">
+          <img src={CoctelCamaron} alt="Cóctel de Camarón" />
+          <h3>Cóctel de Camarón</h3>
+        </div>
+        <div className="receta">
+          <img src={ChupeCentolla} alt="Chupe de Centolla" />
+          <h3>Chupe de Centolla</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mariscos;
