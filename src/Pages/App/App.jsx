c5d149b8b93481a52e644318cbef8911d77a7f8b
Tomas0418 <tomas.giraldoc@comunidad.iush.edu.co>
2025-04-25 12:07:06 -0500

    Organizacion de las paginas en la carpeta Pages

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Signup from "../../components/Signup/Signup.jsx";
+import Login from "../Login/Login.jsx";
+import { useEffect } from "react";
+import MainPage from "../Main/MainPage.jsx";
+import Pastas from "../Pastas/Pastas.jsx";
+import Carnes from "../Carnes/Carnes.jsx";
+import Mariscos from "../Mariscos/Mariscos.jsx";
+
+
+function DynamicTitle() {
+  const location = useLocation();
+
+  useEffect(() => {
+    
+    const titles = {
+      "/": "Recipehub",
+      "/login": "Iniciar sesión",
+      "/signup": "Registro",
+      "/pastas": "Recetas de Pastas", 
+      "/carnes": "Recetas de Carnes",
+      "/mariscos": "Recetas de Mariscos", 
+    };
+
+    document.title = titles[location.pathname] || "Recipehub";
+  }, [location]);
+
+  return null; 
+}
+
+function App() {
+  return (
+    <Router>
+      <DynamicTitle />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pastas" element={<Pastas />} /> 
+        <Route path="/carnes" element={<Carnes />} /> 
+        <Route path="/mariscos" element={<Mariscos />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/Pages/Main/main.jsx
+++ src/Pages/Main/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import MainPage from './MainPage.jsx'
+import '../index.css'
+import App from '../App/App.jsx'
+import MainPage from "./MainPage.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
