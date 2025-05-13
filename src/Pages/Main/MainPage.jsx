ca9957c52893e0fb1b1c2c666d4ca24fe463e934
JuanEEP <juan.escobarp@comunidad.iush.edu.co>
2025-05-13 17:05:05 -0500

    Changes
    recipedetail

--- src/Pages/Main/MainPage.jsx
+++ src/Pages/Main/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Card, CardContent } from "../../components/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
@@ -10,20 +10,29 @@ import CoctelCamaron from "../../Images/coctel-camaron.jpg";
 import CarneAsada from "../../Images/carne-asada.jpg";
 
 export default function MainPage() {
-  const recipes = [
+  const categories = [
     { id: 1, title: "Pastas", image: Spaghetti, path: "/pastas" },
     { id: 2, title: "Mariscos", image: CoctelCamaron, path: "/mariscos" },
     { id: 3, title: "Carnes", image: CarneAsada, path: "/carnes" },
   ];
+
+  const [recetasPublicadas, setRecetasPublicadas] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pfv4sj6v-5000.use2.devtunnels.ms/api/recetas")
+      .then((res) => res.json())
+      .then((data) => setRecetasPublicadas(data))
+      .catch((err) => console.error("Error al cargar recetas:", err));
+  }, []);
 
   return (
     <div className="page-wrapper">
       <Navbar />
       <div className="main-container">
         <main className="main-content">
-          <h1 className="title">Categorias Disponibles</h1>
+          <h1 className="title">Categorías Disponibles</h1>
           <div className="recipes-grid">
-            {recipes.map((recipe) => (
+            {categories.map((recipe) => (
               <Card key={recipe.id} className="recipe-card">
                 <Link to={recipe.path} className="recipe-link">
                   <CardContent>
@@ -38,8 +47,27 @@ export default function MainPage() {
               </Card>
             ))}
           </div>
+
+          <h1 className="title">Recetas Publicadas</h1>
+          <div className="recipes-grid">
+            {recetasPublicadas.map((receta) => (
+              <Card key={receta.id} className="recipe-card">
+                <Link to={`/receta/${receta.id}`} className="recipe-link">
+                  <CardContent>
+                    <img
+                      src={receta.image}
+                      alt={receta.title}
+                      className="recipe-image"
+                    />
+                    <h2 className="recipe-title">{receta.title}</h2>
+                  </CardContent>
+                </Link>
+              </Card>
+            ))}
+          </div>
         </main>
       </div>
+      <Footer />
     </div>
   );
 }
